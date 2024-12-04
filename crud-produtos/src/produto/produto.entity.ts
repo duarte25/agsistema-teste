8e73162c4c0ce9b1aad3e9221cb41a1944d2eee3
duarte25 <duarte.guga2025@gmail.com>
2024-12-04 14:30:14 -0400

    feature: Criando validações no service

--- crud-produtos/src/produto/produto.entity.ts
+++ crud-produtos/src/produto/produto.entity.ts
@@ -1,5 +1,5 @@
 import { Entity, PrimaryGeneratedColumn, Column } from 'typeorm';
-import { IsNotEmpty, IsPositive, Matches } from 'class-validator';
+import { IsNotEmpty, IsPositive } from 'class-validator';
 
 @Entity()
 export class Produto {
@@ -12,9 +12,10 @@ export class Produto {
 
   @Column('decimal', { precision: 10, scale: 2 })
   @IsPositive({ message: 'O preço deve ser um valor positivo.' })
-  @Matches(/^\d+(\.\d{1,2})?$/, {
-    message: 'O preço deve ter no máximo duas casas decimais.',
-  })
+  // Por algum motivo não consegui validar por aqui tive que incluir no controller
+  // @Matches(/^\d+(\.\d{1,2})?$/, {
+  //   message: 'O preço deve ter no máximo duas casas decimais.',
+  // })
   preco: number;
 
   @Column()
--- crud-produtos/src/produto/produto.service.ts
+++ crud-produtos/src/produto/produto.service.ts
@@ -10,29 +10,51 @@ export class ProdutoService {
     private produtoRepository: Repository<Produto>,
   ) {}
 
-  async create(produto: Produto): Promise<Produto> {
-    // Verificação extra de preço
-    if (produto.preco <= 0) {
+  // Função de validação de preço centralizada no serviço, tinha mudado para o controller mas não precisa
+  private validatePreco(preco: number) {
+    const precoStr = preco.toString();
+    const regex = /^\d+(\.\d{1,2})?$/;
+    if (!regex.test(precoStr)) {
+      throw new BadRequestException(
+        'O preço deve ter no máximo duas casas decimais.',
+      );
+    }
+
+    if (preco <= 0) {
       throw new BadRequestException('O preço deve ser maior que zero.');
     }
+  }
+
+  // Criar um novo produto
+  async create(produto: Produto): Promise<Produto> {
+    // Valida preço antes de criar o produto
+    this.validatePreco(produto.preco);
+
     return this.produtoRepository.save(produto);
   }
 
+  // Obter todos os produtos
   async findAll(): Promise<Produto[]> {
     return this.produtoRepository.find();
   }
 
+  // Obter um produto por id
   async findOne(id: number): Promise<Produto> {
     return this.produtoRepository.findOne({
       where: { id },
     });
   }
 
+  // Atualizar um produto com PUT
   async update(id: number, produto: Produto): Promise<Produto> {
+    // Valida preço antes de atualizar o produto, (fundamental)
+    this.validatePreco(produto.preco);
+
     await this.produtoRepository.update(id, produto);
     return this.findOne(id);
   }
 
+  // Remover um produto
   async remove(id: number): Promise<void> {
     await this.produtoRepository.delete(id);
   }

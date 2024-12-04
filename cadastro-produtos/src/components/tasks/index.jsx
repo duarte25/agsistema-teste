c6d3a94d79cc73a4f62e3ed01045b06397bd6802
duarte25 <duarte.guga2025@gmail.com>
2024-12-04 16:30:46 -0400

    feature: Irei apagar todo o código e refazer essa parte do todoList

--- cadastro-produtos/src/components/tasks/index.jsx
+++ cadastro-produtos/src/components/tasks/index.jsx
@@ -2,25 +2,17 @@
 import React from 'react';
 import { FiTrash } from "react-icons/fi";
 
-const TodoItem = ({ todo, toggleComplete, deleteTodo }) => {
+const TodoItem = ({ todo, toggleComplete }) => {
+
+  console.log("TODO", todo)
+
   return (
     <div  >
       <input
         type="checkbox"
-        checked={todo.completed}
-        onChange={() => toggleComplete(todo.id)}
       />
-      <div
-        
-        style={{ textDecoration: todo.completed ? 'line-through' : 'none',
-          opacity: todo.completed ? 0.54 : 1
-         }}
-        onClick={() => toggleComplete(todo.id)}
-      >
-        {todo.text}
-      </div>
-      <div >
-          <FiTrash onClick={() => deleteTodo(todo.id)}/>
+      <div>
+        {todo[0].nome}
       </div>
     </div>
   );
--- cadastro-produtos/src/components/toDolist/index.jsx
+++ cadastro-produtos/src/components/toDolist/index.jsx
@@ -8,111 +8,39 @@ const TodoList = () => {
   const [todos, setTodos] = useState([]);
   const [todoText, setTodoText] = useState('');
   const [isAdding, setIsAdding] = useState(false);
-  const [todoToDelete, setTodoToDelete] = useState(null);
 
   const { data, isLoading } = useQuery({
     queryKey: ["getProduto"],
     queryFn: async () => {
-        const response = await fetchApi("/produtos", "GET");
+      const response = await fetchApi("/produtos", "GET");
 
-        return response;
+      return response;
     }
-});
+  });
 
-console.log("DATA", data)
-
-  useEffect(() => {
-    const storedTodos = JSON.parse(localStorage.getItem('todos'));
-    if (storedTodos && Array.isArray(storedTodos)) {
-      setTodos(storedTodos);
-    }
-  }, []);
-
-  useEffect(() => {
-    if (todos.length > 0) {
-      localStorage.setItem('todos', JSON.stringify(todos));
-    }
-  }, [todos]);
-
-  const confirmAddTodo = () => {
-    if (todoText.trim()) {
-      const newTodo = { id: Date.now(), text: todoText, completed: false };
-      setTodos([...todos, newTodo]);
-      setTodoText('');
-    }
-    setIsAdding(false);
-  };
-
-  const toggleComplete = (id) => {
-    const updatedTodos = todos.map(todo =>
-      todo.id === id ? { ...todo, completed: !todo.completed } : todo
-    );
-    setTodos(updatedTodos);
-  };
-
-  const confirmDeleteTodo = (id) => {
-    const updatedTodos = todos.filter(todo => todo.id !== id);
-    setTodos(updatedTodos);
-    setTodoToDelete(null);
-  };
-
-  const isPopupOpen = isAdding || todoToDelete !== null;
+  console.log("DATA", data)
 
   return (
     <div >
-      {/* Aplicando a classe de opacidade no fundo */}
-      {isPopupOpen && <div></div>}
-
-      {/* Popup para adicionar tarefa */}
       <TodoPopup
-        open={isAdding}
-        onClose={() => setIsAdding(false)}
-        onConfirm={confirmAddTodo}
-        title="Nova tarefa"
-        label="Título"
-        placeholder="Digite"
-        value={todoText}
-        onChange={(e) => setTodoText(e.target.value)}
-        buttonTitleConfirm={"Adicionar"}
+ 
       />
 
       <div >
         <h3>Suas tarefas de hoje</h3>
-        {todos.filter(todo => !todo.completed).map((todo) => (
-          <TodoItem
-            key={todo.id}
-            todo={todo}
+
+        <TodoItem
+            key={data?.data}
+            todo={data?.data}
             toggleComplete={toggleComplete}
-            deleteTodo={() => setTodoToDelete(todo.id)}
           />
-        ))}
-        <h3>Tarefas finalizadas</h3>
-        {todos.filter(todo => todo.completed).map((todo) => (
-          <TodoItem
-            key={todo.id}
-            todo={todo}
-            toggleComplete={toggleComplete}
-            deleteTodo={() => setTodoToDelete(todo.id)}
-          />
-        ))}
+
       </div>
 
-      <button  onClick={() => setIsAdding(true)}>
+      <button onClick={() => setIsAdding(true)}>
         Adicionar nova tarefa
       </button>
 
-      {/* Popup para confirmar a deleção */}
-      <TodoPopup
-        open={todoToDelete !== null}
-        onClose={() => setTodoToDelete(null)}
-        onConfirm={() => confirmDeleteTodo(todoToDelete)}
-        title="Deletar tarefa"
-        label="Tem certeza que você deseja deletar essa tarefa?"
-        value={null}
-        placeholder={null}
-        labelClassName={"color: red"}
-        buttonTitleConfirm={"Deletar"}
-      />
     </div>
   );
 };
